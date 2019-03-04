9b8448b772aff83cb5ce3d9730f51a00bd3dd5e3
greguz <gregolettogiacomo@gmail.com>
2019-03-04 11:09:48 +0100

    Remove stream ctors from exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-export { Duplex, Readable, Transform, Writable } from "stream";
+import { Duplex, Readable, Transform, Writable } from "stream";
+
+// Export just the types, not the constructors
+export type Duplex = Duplex;
+export type Readable = Readable;
+export type Transform = Transform;
+export type Writable = Writable;
+
 export * from "./collect";
 export * from "./duplex";
 export * from "./pump";
