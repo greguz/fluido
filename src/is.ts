b44d2d3b76588b235cf099ab35abb8ca62a7f083
greguz <gregolettogiacomo@gmail.com>
2019-03-06 16:51:28 +0100

    Add test libs

---
+++ src/is.ts
@@ -0,0 +1,21 @@
+import { Duplex, Readable, Transform, Writable } from "stream";
+
+export function isReadable(value: any): value is Readable {
+  return value instanceof Readable;
+}
+
+export function isWritable(value: any): value is Writable {
+  return value instanceof Writable;
+}
+
+export function isDuplex(value: any): value is Duplex {
+  return value instanceof Duplex;
+}
+
+export function isTransform(value: any): value is Transform {
+  return value instanceof Transform;
+}
+
+export function isStream(value: any): value is Readable | Writable {
+  return isReadable(value) || isWritable(value);
+}
