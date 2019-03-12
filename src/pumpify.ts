1ca86e3f061712de94c890d5036cac2aba0cc899
greguz <gregolettogiacomo@gmail.com>
2019-03-12 15:01:03 +0100

    Split pumpify method

--- src/pumpify.ts
+++ src/pumpify.ts
@@ -1,111 +1,31 @@
-import { pipeline, Readable, Transform, Writable } from "stream";
-
-import { isReadable, isTransform } from "./is";
+import { isReadableStrictly, isWritableStrictly } from "./is";
 import { ReadableOptions } from "./readable";
-import { Callback } from "./utils";
+import { readify } from "./readify";
+import { Stream, first, last } from "./utils";
+import { WritableOptions } from "./writable";
+import { writify } from "./writify";
 
 /**
- * Combine an array of streams into a single readable stream
+ * Combine an array of streams into a single stream
  */
 export function pumpify(
-  streams: Array<Readable | Transform>,
-  options?: ReadableOptions
-): Readable {
-  for (let i = 0; i < streams.length; i++) {
-    const stream = streams[i];
-    if (i === 0) {
-      if (!isReadable(stream)) {
-        throw new Error("The first stream must be a readable");
-      }
-    } else {
-      if (!isTransform(stream)) {
-        throw new Error("The other streams must be transforms");
-      }
-    }
-  }
-
-  // Handle special cases
+  streams: Array<Stream>,
+  options?: ReadableOptions | WritableOptions
+): Stream {
   if (streams.length <= 0) {
-    // Return an empty readable
-    return new Readable({
-      ...options,
-      read() {
-        this.push(null);
-      }
-    });
+    throw new Error("Expected at least one stream");
   } else if (streams.length === 1) {
-    // A pipeline of a single stream, is the stream itself
-    return streams[0] as Readable;
+    return streams[0];
   }
 
-  // Clone the streams array
-  const line: any[] = [...streams];
+  const asReadable = isReadableStrictly(first(streams));
+  const asWritable = isWritableStrictly(last(streams));
 
-  // Helpers
-  let target: Writable | undefined;
-  let next: Callback | undefined;
-  let destroyed: Callback | undefined;
+  if (asReadable === asWritable) {
+    throw new Error("Unable to guess the resulting stream");
+  }
 
-  // Return the composed readable stream
-  return new Readable({
-    ...options,
-    read() {
-      // This readable
-      const readable = this;
-
-      if (!target) {
-        // Create a writable to append to the pipeline that writes to this readable
-        target = new Writable({
-          // Inherit the mode from the readable options
-          objectMode: options && options.objectMode === true,
-          // What is written to this stream, is pushed to the readable
-          write(chunk: any, encoding: string, callback: Callback) {
-            if (readable.push(chunk, encoding)) {
-              // Everything is flowing, keep pushing
-              callback();
-            } else {
-              // Backpressure kicks in, save the callback for the next "read"
-              next = callback;
-            }
-          }
-        });
-
-        // Push our writable and the final callback
-        line.push(target, (err?: any) => {
-          // Flag the execution end
-          target = undefined;
-
-          if (destroyed) {
-            // We are here because of a forced destruction
-            destroyed(err);
-          } else if (err) {
-            // Errored finish, handle the error just like a normal readable
-            readable.emit("error", err);
-          } else {
-            // No error, clean finish
-            readable.push(null);
-          }
-        });
-
-        // Run the pipeline
-        pipeline.apply(null, line);
-      } else if (next) {
-        // Handle the backpressure mechanism
-        const callback = next;
-        next = undefined;
-        callback();
-      }
-    },
-    destroy(err: any, callback: Callback) {
-      if (target) {
-        // Save the destroy callback
-        destroyed = callback;
-        // Destroy our writable so the pipeline will stop
-        target.destroy(err);
-      } else {
-        // Nothing is running (not sure why)
-        callback(err);
-      }
-    }
-  });
+  return asReadable
+    ? readify(streams as any[], options)
+    : writify(streams as any[], options);
 }
---
+++ src/readify.ts
@@ -0,0 +1,76 @@
+import { pipeline, Readable, Transform } from "stream";
+
+import { isReadable, isTransform } from "./is";
+import { ReadableOptions } from "./readable";
+import { Callback, last } from "./utils";
+import { voidReadable } from "./void";
+
+/**
+ * Combine an array of streams into a single readable stream
+ */
+export function readify(
+  streams: Array<Readable | Transform>,
+  options?: ReadableOptions
+): Readable {
+  // Validate streams
+  for (let i = 0; i < streams.length; i++) {
+    if (i === 0) {
+      if (!isReadable(streams[i])) {
+        throw new TypeError("First stream must be a readable stream");
+      }
+    } else {
+      if (!isTransform(streams[i])) {
+        throw new TypeError("Other streams must be transform streams");
+      }
+    }
+  }
+
+  // Handle simple cases
+  if (streams.length <= 0) {
+    return voidReadable(options);
+  } else if (streams.length === 1) {
+    return streams[0];
+  }
+
+  let source: Readable | undefined;
+  let cbDestroy: Callback | undefined;
+
+  return new Readable({
+    ...options,
+    read() {
+      if (!source) {
+        source = last(streams) as Readable;
+
+        source.on("data", (chunk: any) => {
+          if (!this.push(chunk)) {
+            (source as Readable).pause();
+          }
+        });
+
+        const callback = (err?: any) => {
+          source = undefined;
+
+          if (cbDestroy) {
+            cbDestroy(err);
+          } else if (err) {
+            this.emit("error", err);
+          } else {
+            this.push(null);
+          }
+        };
+
+        pipeline.apply(null, [...streams, callback]);
+      } else {
+        source.resume();
+      }
+    },
+    destroy(err: any, callback: Callback) {
+      if (!source) {
+        callback(err);
+      } else {
+        cbDestroy = callback;
+        source.destroy(err);
+      }
+    }
+  });
+}
