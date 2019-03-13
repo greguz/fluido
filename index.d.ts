a5a4082019e925eba51edc04f2dcc90e96a183ba
greguz <gregolettogiacomo@gmail.com>
2019-03-13 11:02:28 +0100

    Configure Standard JS env

---
+++ index.d.ts
@@ -0,0 +1,135 @@
+/// <reference types="node" />
+
+import {
+  Duplex as _Duplex,
+  Readable as _Readable,
+  Transform as _Transform,
+  Writable as _Writable
+} from 'stream'
+
+export declare type Callback = (err?: any) => any
+
+export interface Readable extends _Readable {
+  readableFlowing: null | false | true
+}
+export interface ReadableOptions {
+  autoDestroy?: boolean
+  encoding?: string
+  highWaterMark?: number
+  objectMode?: boolean
+}
+export interface ReadableMethods<T = any> {
+  read?(size: number): any
+  destroy?(err: any, callback: Callback): any
+}
+export declare function readable<T = any>(
+  options?: ReadableOptions & ReadableMethods<T>
+): Readable
+
+export interface Writable extends _Writable {}
+export interface WritableOptions {
+  autoDestroy?: boolean
+  decodeStrings?: boolean
+  defaultEncoding?: string
+  emitClose?: boolean
+  highWaterMark?: number
+  objectMode?: boolean
+}
+export interface WritableEntry<T = any> {
+  chunk: T
+  encoding: string
+}
+export interface WritableMethods<T = any> {
+  write?(this: Writable, chunk: T, encoding: string, callback: Callback): any
+  writev?(
+    this: Writable,
+    entries: Array<WritableEntry<T>>,
+    callback: Callback
+  ): any
+  final?(this: Writable, callback: Callback): any
+  destroy?(this: Writable, err: any, callback: Callback): any
+}
+export declare function writable<T = any>(
+  options?: WritableOptions & WritableMethods<T>
+): Writable
+
+export declare type Stream = Readable | Writable
+
+export interface Duplex extends _Duplex {
+  readableFlowing: null | false | true
+}
+export declare type DuplexOptions = ReadableOptions & WritableOptions
+export declare type DuplexMethods<R = any, W = any> = ReadableMethods<R> &
+  WritableMethods<W>
+export declare function duplex<R = any, W = any>(
+  options?: DuplexOptions & DuplexMethods<R, W>
+): Duplex
+
+export interface Transform extends _Transform {
+  readableFlowing: null | false | true
+}
+export declare type TransformCallback<T = any> = (err?: any, data?: T) => any
+export interface TransformOptions extends ReadableOptions, WritableOptions {
+  concurrency?: number
+}
+export interface TransformMethods<R = any, W = any> {
+  transform?(
+    this: Transform,
+    chunk: R,
+    encoding: string,
+    callback: TransformCallback<W>
+  ): any
+  flush?(this: Transform, callback: TransformCallback<W>): any
+  destroy?(this: Transform, err: any, callback: (err?: any) => any): any
+}
+export declare function transform<R = any, W = any>(
+  options?: TransformOptions & TransformMethods<R, W>
+): Transform
+
+export declare function collect(
+  encoding?: string | false | undefined
+): Transform
+
+export declare function finished(stream: Stream): Promise<void>
+export declare function finished(stream: Stream, callback: Callback): void
+
+export declare function subscribe(
+  ...args: Array<Readable | Transform>
+): Promise<any>
+export declare function subscribe(
+  ...args: Array<Readable | Transform | Callback>
+): void
+
+export declare function readify(
+  streams: Array<Readable | Transform>,
+  options?: ReadableOptions
+): Readable
+
+export declare function writify(
+  streams: Array<Writable | Transform>,
+  options?: WritableOptions
+): Writable
+
+export declare function pumpify(
+  streams: Stream[],
+  options?: ReadableOptions | WritableOptions
+): Stream
+
+export declare function duplexify(
+  readable: Readable,
+  writable: Writable,
+  options?: DuplexOptions
+): Duplex
+
+export declare function isReadable(value: any): value is Readable
+export declare function isWritable(value: any): value is Writable
+export declare function isDuplex(value: any): value is Duplex
+export declare function isTransform(value: any): value is Transform
+export declare function isStream(value: any): value is Stream
+export declare function isReadableStrictly(value: any): value is Readable
+export declare function isWritableStrictly(value: any): value is Writable
+export declare function isDuplexStrictly(value: any): value is Duplex
+export declare function isClosed(stream: Stream): boolean
+
+export declare function pump(...args: Stream[]): Promise<any>
+export declare function pump(...args: Array<Stream | Callback>): void
