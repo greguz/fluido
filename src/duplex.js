a357dec04be295c57504e46eb0b30e8af083e8c6
greguz <gregolettogiacomo@gmail.com>
2019-06-26 20:23:25 +0200

    Support readable callback with duplex streams

--- src/duplex.js
+++ src/duplex.js
@@ -1,10 +1,14 @@
 import { Duplex } from 'stream'
 import { voidRead, voidWrite } from './internal/void'
+import wrap from './internal/from'
 
-export function duplex (options) {
+export function duplex (options = {}) {
+  const read = options.read || voidRead
+  const write = options.write || voidWrite
+
   return new Duplex({
-    read: voidRead,
-    write: voidWrite,
-    ...options
+    ...options,
+    read: read.length >= 2 ? wrap(read) : read,
+    write
   })
 }
--- src/readable.js
+++ src/readable.js
@@ -1,35 +1,6 @@
 import { Readable } from 'stream'
 import { voidRead } from './internal/void'
-
-function wrap (read) {
-  let reading = false
-
-  return function wrapped (size) {
-    if (reading) {
-      return
-    }
-    reading = true
-
-    read.call(this, size, (err, data) => {
-      if (err) {
-        process.nextTick(() => this.emit('error', err))
-        return
-      }
-
-      reading = false
-
-      const ended = this._readableState.ended === true
-
-      const flowing = data === undefined
-        ? this.readableFlowing === true
-        : this.push(data)
-
-      if (!ended && flowing) {
-        this._read(size)
-      }
-    })
-  }
-}
+import wrap from './internal/from'
 
 export function readable (options = {}) {
   const read = options.read || voidRead
