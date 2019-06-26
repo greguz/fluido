73456c0582aba9571bc09d123e9e377c8fbcbc9f
greguz <gregolettogiacomo@gmail.com>
2019-06-26 15:15:17 +0200

    Remove empty tests

--- test/pump.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava'
-import { pump } from '../index.js'
-
-test('todo', t => {
-  t.true(typeof pump === 'function')
-  t.pass()
-})
--- test/subscribe.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava'
-import { subscribe } from '../index.js'
-
-test('todo', t => {
-  t.true(typeof subscribe === 'function')
-  t.pass()
-})
