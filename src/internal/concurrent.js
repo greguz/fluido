6d5a84889b30e491b2faeb3a9decf0f2697b25f1
greguz <gregolettogiacomo@gmail.com>
2019-06-27 12:05:04 +0200

    Remove duplicated code

---
+++ src/internal/concurrent.js
@@ -0,0 +1,90 @@
+export default function concurrent (concurrency, _transform, _flush, _destroy) {
+  if (!Number.isInteger(concurrency) || concurrency <= 0) {
+    throw new TypeError('Concurrency must be a positive number')
+  }
+  if (!_transform) {
+    throw new TypeError('Transform function is required')
+  }
+
+  // Active jobs counter
+  let jobs = 0
+
+  // First encountered error
+  let error
+
+  // Require more chunks callback
+  let next
+
+  // End stream callback
+  let close
+
+  function transform (chunk, encoding, callback) {
+    if (!error) {
+      job.call(this, chunk, encoding)
+    }
+
+    if (!error && jobs < concurrency) {
+      // Keep requiring more chunks
+      callback()
+    } else {
+      // Save the current callback for jobs completion
+      next = callback
+    }
+  }
+
+  function job (chunk, encoding) {
+    jobs++
+
+    _transform.call(this, chunk, encoding, (err, data) => {
+      jobs--
+
+      error = error || err
+
+      if (!error && data !== undefined) {
+        this.push(data)
+      }
+
+      if (jobs <= 0) {
+        const callback = close || next
+        if (callback) {
+          callback(error)
+        } else if (error) {
+          this.emit('error', error)
+        }
+      } else if (!error && next) {
+        const callback = next
+        next = undefined
+        callback()
+      }
+    })
+  }
+
+  function flush (callback) {
+    // Build close callback
+    close = err => {
+      if (err || !_flush) {
+        callback(err)
+      } else {
+        _flush.call(this, callback)
+      }
+    }
+
+    if (jobs <= 0) {
+      close()
+    }
+  }
+
+  function destroy (err, callback) {
+    // Ensure error to stop data flowing
+    error = error || err || new Error('Premature close')
+
+    // Build close callback
+    close = _destroy ? _destroy.bind(this, error, callback) : callback
+
+    if (jobs <= 0) {
+      close()
+    }
+  }
+
+  return [transform, flush, destroy]
+}
--- src/writable.js
+++ src/writable.js
@@ -1,10 +1,22 @@
 import { Writable } from 'stream'
-import { concurrent } from './internal/concurrentWritable'
+import concurrent from './internal/concurrent'
 import { voidWrite } from './internal/void'
 
-export function writable (options) {
-  if (options && options.concurrency !== undefined) {
-    return concurrent(options)
+export function writable (options = {}) {
+  if (options.concurrency) {
+    const [write, final, destroy] = concurrent(
+      options.concurrency,
+      options.write,
+      options.final,
+      options.destroy
+    )
+
+    return new Writable({
+      ...options,
+      write,
+      final,
+      destroy
+    })
   } else {
     return new Writable({ write: voidWrite, ...options })
   }
