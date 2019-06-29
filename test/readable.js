eb2c8a451131897ff5174849625cf5699cbd2a3d
greguz <gregolettogiacomo@gmail.com>
2019-06-29 09:39:51 +0200

    Add callback mode tests

---
+++ test/readable.js
@@ -0,0 +1,34 @@
+import test from 'ava'
+import { Writable, pipeline } from 'stream'
+import { readable } from '../index.js'
+
+test.cb('readable callback mode', t => {
+  let counter = 0
+
+  const source = readable({
+    objectMode: true,
+    read (size, callback) {
+      for (let i = 97; i <= 122; i++) {
+        this.push(String.fromCharCode(i))
+      }
+      if (++counter >= 4) {
+        this.push(null)
+      }
+      setTimeout(callback, 20)
+    }
+  })
+
+  const target = new Writable({
+    objectMode: true,
+    write (chunk, encoding, callback) {
+      setTimeout(callback, 50)
+    }
+  })
+
+  pipeline(source, target, err => {
+    if (!err) {
+      t.is(counter, 4)
+    }
+    t.end(err)
+  })
+})
