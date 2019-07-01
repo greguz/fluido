c1a90f1b49bc85736ceb249bed77d94ed6e24abb
greguz <gregolettogiacomo@gmail.com>
2019-07-01 10:21:29 +0200

    Improve writify function

--- src/internal/utils.js
+++ src/internal/utils.js
@@ -4,10 +4,6 @@ export function isFunction (value) {
   return typeof value === 'function'
 }
 
-export function first (arr) {
-  return arr[0]
-}
-
 export function last (arr) {
   return arr.length > 0 ? arr[arr.length - 1] : undefined
 }
