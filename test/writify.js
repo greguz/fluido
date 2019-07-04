4e57196f2a3bb22c45ef2af43f94daebe1273200
greguz <gregolettogiacomo@gmail.com>
2019-07-04 09:55:33 +0200

    Add more tests

---
+++ test/writify.js
@@ -0,0 +1,44 @@
+import test from 'ava'
+import { Readable, Transform, Writable, pipeline } from 'stream'
+import { writify } from '../index.js'
+
+test.cb('writify', t => {
+  let rc = 0
+  const head = new Readable({
+    highWaterMark: 10,
+    read () {
+      let flowing = true
+      while (flowing && rc < 100) {
+        flowing = this.push(rc.toString())
+        rc++
+      }
+      if (rc >= 100) {
+        this.push(null)
+      }
+    }
+  })
+
+  const body = new Transform({
+    highWaterMark: 10,
+    transform (chunk, encoding, callback) {
+      callback(null, chunk)
+    }
+  })
+
+  let wc = 0
+  const tail = new Writable({
+    highWaterMark: 10,
+    write (chunk, encoding, callback) {
+      wc++
+      setTimeout(callback, 10)
+    }
+  })
+
+  pipeline(head, writify([body, tail]), err => {
+    if (!err) {
+      t.is(rc, 100)
+      t.is(wc, 100)
+    }
+    t.end(err)
+  })
+})
