0f922f59b1c55e6c2a70762650c6d1afcba1782b
greguz <gregolettogiacomo@gmail.com>
2019-10-07 22:27:21 +0200

    Fix collect

--- src/collect.js
+++ src/collect.js
@@ -2,34 +2,34 @@ import { transform } from './transform'
 
 function asString (chunks, encoding, callback) {
   let result = ''
-
   for (const chunk of chunks) {
-    if (!Buffer.isBuffer(chunk)) {
-      process.nextTick(callback, new TypeError('Chunk must be buffer or string'))
-      return
+    if (Buffer.isBuffer(chunk)) {
+      result += chunk.toString(encoding)
+    } else if (typeof chunk === 'string') {
+      result += chunk
+    } else {
+      return callback(new TypeError('Chunk must be buffer or string'))
     }
-    result += chunk.toString(encoding)
   }
-
-  process.nextTick(callback, null, result)
+  callback(null, result)
 }
 
 function asBuffer (chunks, callback) {
   let result = Buffer.from([])
-
   for (const chunk of chunks) {
-    if (!Buffer.isBuffer(chunk)) {
-      process.nextTick(callback, new TypeError('Chunk must be buffer or string'))
-      return
+    if (Buffer.isBuffer(chunk)) {
+      result = Buffer.concat([result, chunk])
+    } else if (typeof chunk === 'string') {
+      result = Buffer.concat([result, Buffer.from(chunk)])
+    } else {
+      return callback(new TypeError('Chunk must be buffer or string'))
     }
-    result = Buffer.concat([result, chunk])
   }
-
-  process.nextTick(callback, null, result)
+  callback(null, result)
 }
 
 function asIs (chunks, callback) {
-  process.nextTick(callback, null, chunks)
+  callback(null, chunks)
 }
 
 export function collect (encoding) {
@@ -38,18 +38,23 @@ export function collect (encoding) {
   return transform({
     objectMode: true,
     transform (chunk, ce, callback) {
-      chunks.push(typeof chunk === 'string' ? Buffer.from(chunk, ce) : chunk)
-      callback()
-    },
-    flush (callback) {
       if (encoding === undefined) {
-        if (Buffer.isBuffer(chunks[0])) {
+        if (Buffer.isBuffer(chunk)) {
           encoding = 'buffer'
+        } else if (typeof chunk === 'string') {
+          encoding = 'utf8'
         } else {
           encoding = false
         }
       }
-
+      chunks.push(
+        typeof chunk === 'string' && encoding === 'buffer'
+          ? Buffer.from(chunk, ce)
+          : chunk
+      )
+      callback()
+    },
+    flush (callback) {
       if (encoding === 'buffer') {
         asBuffer(chunks, callback)
       } else if (typeof encoding === 'string') {
@@ -57,7 +62,6 @@ export function collect (encoding) {
       } else {
         asIs(chunks, callback)
       }
-
       chunks = []
     },
     destroy (err, callback) {
