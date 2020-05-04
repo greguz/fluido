b9260a020c0d38bcb751385bcddce214fc8cd4d4
greguz <gregolettogiacomo@gmail.com>
2020-05-04 10:56:17 +0200

    Change finished signature

--- src/finished.js
+++ src/finished.js
@@ -1,12 +1,4 @@
-import handleInternal from './internal/handle'
-import { isFunction, last } from './internal/utils'
+import stream from 'stream'
+import { fromCallback } from 'universalify'
 
-export function finished (...args) {
-  if (!isFunction(last(args))) {
-    throw new TypeError('Expected callback')
-  }
-  if (args.length <= 1) {
-    throw new Error('Expected at least one stream')
-  }
-  return handleInternal(args, false, args.pop())
-}
+export const finished = fromCallback(stream.finished)
