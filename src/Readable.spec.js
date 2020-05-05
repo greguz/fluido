aed00298b1dbf8409a3d7e4a2f52d74516f010b9
greguz <gregolettogiacomo@gmail.com>
2020-05-05 16:54:11 +0200

    Add some basic tests

--- src/Readable.spec.js
+++ src/Readable.spec.js
@@ -1,5 +1,58 @@
 import test from 'ava'
 
-test('foo', t => {
-  t.pass()
+import { Writable, pipeline } from 'readable-stream'
+
+import { Readable } from './Readable'
+
+test('Readable read', t => {
+  t.plan(21)
+  const steps = 10
+  let index = 0
+  const stream = new Readable({
+    objectMode: true,
+    read () {
+      t.pass()
+      this.push(index < steps ? index++ : null)
+    }
+  })
+  let value = stream.read()
+  while (value !== null) {
+    t.true(typeof value === 'number')
+    value = stream.read()
+  }
+})
+
+test.cb('Readable asyncRead', t => {
+  t.plan(23)
+
+  const steps = 10
+  let ri = 0
+  let wi = 0
+
+  pipeline(
+    new Readable({
+      objectMode: true,
+      asyncRead (size, callback) {
+        t.pass()
+        setTimeout(
+          () => callback(null, ri < steps ? ri++ : null),
+          10
+        )
+      }
+    }),
+    new Writable({
+      objectMode: true,
+      write (chunk, encoding, callback) {
+        t.is(chunk, wi++)
+        callback()
+      }
+    }),
+    err => {
+      if (!err) {
+        t.is(ri, 10)
+        t.is(wi, 10)
+      }
+      t.end(err)
+    }
+  )
 })
