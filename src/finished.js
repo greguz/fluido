0b12829820c4b9cf20bcc1c555ccaf81c8128cb3
greguz <gregolettogiacomo@gmail.com>
2020-05-07 14:22:18 +0200

    Remove famous lib aliases

--- src/finished.js
+++ src/finished.js
@@ -2,5 +2,3 @@ import stream from 'readable-stream'
 import { fromCallback } from 'universalify'
 
 export const finished = fromCallback(stream.finished)
-
-export const eos = finished
--- src/pipeline.js
+++ src/pipeline.js
@@ -2,5 +2,3 @@ import stream from 'readable-stream'
 import { fromCallback } from 'universalify'
 
 export const pipeline = fromCallback(stream.pipeline)
-
-export const pump = pipeline
