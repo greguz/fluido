7a0aef758af1698bac572271bfc6be42d197d2f4
greguz <gregolettogiacomo@gmail.com>
2020-05-12 13:36:42 +0200

    Add more tests

---
+++ src/Transform.spec.js
@@ -0,0 +1,54 @@
+import test from 'ava'
+
+import { Readable, Writable, pipeline } from 'readable-stream'
+import { fromCallback } from 'universalify'
+
+import { Transform } from './Transform'
+
+const uDelay = fromCallback(
+  function delay (ms, callback) {
+    setTimeout(callback, ms, null)
+  }
+)
+
+const uPipeline = fromCallback(pipeline)
+
+test('Transform concurrency', async t => {
+  t.plan(100)
+
+  const concurrency = 4
+  const items = 100
+
+  let jobs = 0
+  let index = 0
+
+  await uPipeline(
+    Readable.from(new Array(items).fill(1)),
+    new Transform({
+      concurrency,
+      objectMode: true,
+      async transform () {
+        if (jobs < 0 || jobs >= concurrency) {
+          t.fail()
+        }
+
+        const i = index++
+        if (i < concurrency) {
+          t.is(jobs, i)
+        } else {
+          t.is(jobs, concurrency - 1)
+        }
+
+        jobs++
+        await uDelay(10)
+        jobs--
+      }
+    }),
+    new Writable({
+      objectMode: true,
+      write (chunk, encoding, callback) {
+        callback()
+      }
+    })
+  )
+})
---
+++ src/Writable.spec.js
@@ -0,0 +1,115 @@
+import test from 'ava'
+
+import { Readable, pipeline } from 'readable-stream'
+import { fromCallback } from 'universalify'
+
+import { Writable } from './Writable'
+
+const uDelay = fromCallback(
+  function delay (ms, callback) {
+    setTimeout(callback, ms, null)
+  }
+)
+
+const uPipeline = fromCallback(pipeline)
+
+test.cb('Writable write callback', t => {
+  t.plan(100)
+  uPipeline(
+    Readable.from(new Array(100).fill(1)),
+    new Writable({
+      objectMode: true,
+      write (chunk, encoding, callback) {
+        t.is(chunk, 1)
+        uDelay(10, callback)
+      }
+    }),
+    t.end
+  )
+})
+
+test('Writable write promise', async t => {
+  t.plan(100)
+  await uPipeline(
+    Readable.from(new Array(100).fill(1)),
+    new Writable({
+      objectMode: true,
+      async write (chunk) {
+        t.is(chunk, 1)
+        await uDelay(10)
+      }
+    })
+  )
+})
+
+test.cb('Writable final callback', t => {
+  t.plan(1)
+  let count = 0
+  uPipeline(
+    Readable.from(new Array(100).fill(1)),
+    new Writable({
+      objectMode: true,
+      write (chunk, encoding, callback) {
+        count += chunk
+        callback()
+      },
+      final (callback) {
+        t.is(count, 100)
+        uDelay(10, callback)
+      }
+    }),
+    t.end
+  )
+})
+
+test('Writable final promise', async t => {
+  t.plan(1)
+  let count = 0
+  await uPipeline(
+    Readable.from(new Array(100).fill(1)),
+    new Writable({
+      objectMode: true,
+      async write (chunk) {
+        count += chunk
+      },
+      async final () {
+        t.is(count, 100)
+        await uDelay(10)
+      }
+    })
+  )
+})
+
+test('Writable concurrency', async t => {
+  t.plan(100)
+
+  const concurrency = 4
+  const items = 100
+
+  let jobs = 0
+  let index = 0
+
+  await uPipeline(
+    Readable.from(new Array(items).fill(1)),
+    new Writable({
+      concurrency,
+      objectMode: true,
+      async write () {
+        if (jobs < 0 || jobs >= concurrency) {
+          t.fail()
+        }
+
+        const i = index++
+        if (i < concurrency) {
+          t.is(jobs, i)
+        } else {
+          t.is(jobs, concurrency - 1)
+        }
+
+        jobs++
+        await uDelay(10)
+        jobs--
+      }
+    })
+  )
+})
